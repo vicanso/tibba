ff2e2b7a1999fdf22fca5012a61d57cce481df0a
vicanso <tree.xie@outlook.com>
2023-01-02 19:58:02 +0800

    feat: support incr function

--- src/cache/redis_client.rs
+++ src/cache/redis_client.rs
@@ -1,7 +1,10 @@
 use once_cell::sync::OnceCell;
 use r2d2::Pool;
-use redis::{Client, ConnectionLike};
-use std::time::Duration;
+use redis::{Client, Commands, ConnectionLike};
+use std::{
+    ops::{Deref, DerefMut},
+    time::Duration,
+};
 
 use crate::{config::must_new_redis_config, error::HTTPResult};
 
@@ -42,17 +45,39 @@ impl RedisCache {
             ttl: Duration::from_secs(5 * 60),
         })
     }
-    /// Lock a key with ttl, if ttl is none, the default ttl will be used.
+    /// Lock a key with ttl, if ttl is none,
+    /// the default ttl will be used.
     pub fn lock(&self, key: String, ttl: Option<Duration>) -> HTTPResult<bool> {
         let mut conn = self.pool.get()?;
-        let result = conn.req_command(
-            redis::cmd("SET")
-                .arg(key)
-                .arg(true)
-                .arg("NX")
-                .arg("EX")
-                .arg(ttl.unwrap_or(self.ttl).as_secs()),
-        )?;
-        Ok(result == redis::Value::Okay)
+        let result = redis::cmd("SET")
+            .arg(key)
+            .arg(true)
+            .arg("NX")
+            .arg("EX")
+            .arg(ttl.unwrap_or(self.ttl).as_secs())
+            .query::<bool>(conn.deref_mut())?;
+        Ok(result)
+    }
+    /// Del a key from cache
+    pub fn del(&self, key: String) -> HTTPResult<()> {
+        let mut conn = self.pool.get()?;
+        let _ = conn.del(key)?;
+        Ok(())
+    }
+    /// Increase the value of key
+    pub fn incr(&self, key: String, delta: i64, ttl: Option<Duration>) -> HTTPResult<i64> {
+        let mut conn = self.pool.get()?;
+        let (_, count) = redis::pipe()
+            .cmd("SET")
+            .arg(key.clone())
+            .arg(0)
+            .arg("NX")
+            .arg("EX")
+            .arg(ttl.unwrap_or(self.ttl).as_secs())
+            .cmd("INCRBY")
+            .arg(key)
+            .arg(delta)
+            .query::<(bool, i64)>(conn.deref_mut())?;
+        Ok(count)
     }
 }
